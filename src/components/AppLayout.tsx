9f8ba37ff8e5ed6cd6b840ec02b6e793d235d462
Jesus Ordosgoitty <jesuodz@gmail.com>
2026-01-23 01:05:00 -0400

    increase app layout padding

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -12,7 +12,7 @@ export const AppLayout = ({ children, className }: AppLayoutProps) => {
     <div className="min-h-screen bg-background flex flex-col">
       <div
         className={cn(
-          "container mx-auto min-h-screen bg-background relative flex flex-col lg:pt-10 pb-24",
+          "container mx-auto min-h-screen bg-background relative flex flex-col pt-6 lg:pt-10 pb-24",
           className
         )}
       >
